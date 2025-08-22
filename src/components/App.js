a10eb19ee0c3a3a17562f724f86e65cf31ed5aba
HERCULES0000 <anuragbachheti1999@gmail.com>
2025-08-22 14:20:46 +0530

    code completed again

--- src/components/App.js
+++ src/components/App.js
@@ -20,7 +20,6 @@ const App = () => {
   return (
     <div>
       <h1>Parent Component</h1>
-      <h2>Child Component</h2>
       <TodoList todos={todos} handleComplete={handleComplete}/>
     </div>
   );
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,20 +3,24 @@ import React from 'react';
 const TodoList = ({ todos, handleComplete }) => {
 
     return (
-        <ul>
-            {todos.map(todo =>(
-                <li key={todo.id}>
-                    <span style={{ textDecoration: todo.complete ? "line-through" : "none" }}></span>
+
+        <div>
+            <h2>Child Component</h2>
+            <ul>
+                {todos.map(todo => (
+                    <li key={todo.id}>
+                        <span style={{ textDecoration: todo.complete ? "line-through" : "none" }}></span>
                         {todo.text}
-                    <span/>
-                    {!todo.complete && (
-                        <button onClick={() => handleComplete(todo.id)}>
-                            Complete
-                        </button>
-                    )}
-                </li>
-            ))}
-        </ul>
+                        <span />
+                        {!todo.complete && (
+                            <button onClick={() => handleComplete(todo.id)}>
+                                Complete
+                            </button>
+                        )}
+                    </li>
+                ))}
+            </ul>
+        </div>
     );
 };
 
